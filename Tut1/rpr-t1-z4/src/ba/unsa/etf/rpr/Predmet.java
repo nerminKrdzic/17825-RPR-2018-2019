f370e748d8212d1eace0bbee7b3d9e9e80568ac8
Nermin Krdzic <nkrdzic1@etf.unsa.ba>
2018-10-15 20:57:37 +0200

    Dodao cetvrti zadatak... unesena su 3 studenta i 2 predmeta u kodu

---
+++ Tut1/rpr-t1-z4/src/ba/unsa/etf/rpr/Predmet.java
@@ -0,0 +1,105 @@
+package ba.unsa.etf.rpr;
+
+public class Predmet {
+
+    private String nazivPredmeta = null;
+    private Integer sifraPredmeta = null;
+    private int maxBrojStudenata = 0;
+    private Student[] studenti = null;
+
+    public String getNazivPredmeta() {
+        return nazivPredmeta;
+    }
+
+    public void setNazivPredmeta(String nazivPredmeta) {
+        this.nazivPredmeta = nazivPredmeta;
+    }
+
+    public Integer getSifraPredmeta() {
+        return sifraPredmeta;
+    }
+
+    public void setSifraPredmeta(Integer sifraPredmeta) {
+        this.sifraPredmeta = sifraPredmeta;
+    }
+
+    public int getMaxBrojStudenata() {
+        return maxBrojStudenata;
+    }
+
+    public Student[] getStudenti() {
+        return studenti;
+    }
+
+    public void setStudenti(Student[] studenti) {
+        this.studenti = studenti;
+    }
+
+    public Predmet(String nazivPredmeta, Integer sifraPredmeta, int maxBrojStudenata){
+        this.nazivPredmeta = nazivPredmeta;
+        this.sifraPredmeta = sifraPredmeta;
+        this.maxBrojStudenata = maxBrojStudenata;
+    }
+
+    public Boolean Upisan(Student student){
+        for(Student s : studenti)
+            if(s.getBrojIndexa().equals(student.getBrojIndexa())) return true;
+        return false;
+    }
+
+    public void Upisi(Student student){
+        if(studenti == null || studenti.length == 0){
+            studenti = new Student[1];
+            studenti[0] = student;
+            return;
+        }
+        if(studenti.length == maxBrojStudenata){
+            System.out.println("Izabrani predmet je popunjen");
+            return;
+        }
+        if(Upisan(student)){
+            System.out.println("Student je vec upisan na navedeni predmet!");
+            return;
+        }
+        Student[] novi = new Student[studenti.length + 1];
+        for(int i = 0; i < studenti.length; i++) novi[i] = studenti[i];
+        novi[studenti.length] = student;
+        this.studenti = novi;
+    }
+    public void Ispisi(Student student){
+        if(studenti == null || studenti.length == 0){
+            System.out.println("Predmet nema studenata!");
+            return;
+        }
+        if(!Upisan(student)){
+            System.out.println("Student nije upisan na navedeni predmet!");
+            return;
+        }
+        Student[] novi = new Student[studenti.length - 1];
+        int j = 0;
+        for(int i = 0; i < novi.length; i++){
+            if(!student.getBrojIndexa().equals(studenti[j].getBrojIndexa()))
+                novi[i] = studenti[j];
+            else j++;
+        }
+        this.studenti = novi;
+    }
+
+    @Override
+    public String toString() {
+        String lista = "1. ";
+        int i = 1;
+        for(Student s : studenti){
+            if(s == null) break;
+            if(i == 1) {
+                lista += s.toString();
+                i++;
+            }
+            else {
+                lista += Integer.toString(i) + ". " + s.toString();
+                i++;
+            }
+        }
+        return lista;
+    }
+}
---
+++ Tut1/rpr-t1-z4/src/ba/unsa/etf/rpr/Program.java
@@ -0,0 +1,308 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Program {
+    private Predmet[] predmeti = null;
+    private Student[] studenti = null;
+
+    public Boolean ProvjeriPodatkePredmeta(String nazivPredmeta, Integer sifraPredmeta, int maxBrojStudenata){
+        if(nazivPredmeta.isEmpty()){
+            System.out.println("Niste unijeli naziv oredmeta!");
+            return false;
+        }
+        if(predmeti == null || predmeti.length == 0) return true;
+        for(Predmet p : predmeti) {
+            if (p.getNazivPredmeta().equalsIgnoreCase(nazivPredmeta)) {
+                System.out.println("Predmet vec postoji!");
+                return false;
+            } else if (p.getSifraPredmeta().equals(sifraPredmeta)) {
+                System.out.println("Poklapanje sifre predmeta!");
+                return false;
+            }
+        }
+        if(maxBrojStudenata == 0){
+            System.out.println("Maximalan broj studenata na predmetu ne moze biti nula!");
+            return false;
+        }
+
+        return true;
+    }
+    public Boolean ProvjeriPodatkeStudenta(String prezime, String ime, Integer brojIndexa){
+        if(prezime.isEmpty()){
+            System.out.println("Niste unijeli prezime studenta!");
+            return false;
+        }
+        if(ime.isEmpty()){
+            System.out.println("Niste unijeli ime studenta!");
+            return false;
+        }
+        if(studenti == null || studenti.length == 0)
+            return true;
+        for(Student s : studenti)
+            if(s.getBrojIndexa().equals(brojIndexa)){
+                System.out.println("Poklapanje broja indexa!");
+                return false;
+            }
+        return true;
+    }
+    public Boolean ImaPredmet(String nazivPredmeta){
+        if(predmeti == null || predmeti.length == 0) return false;
+        for(Predmet p : predmeti)
+            if(p.getNazivPredmeta().equalsIgnoreCase(nazivPredmeta)){
+                return true;
+            }
+            return false;
+    }
+    public Boolean ImaStudent(Integer brojIndexa){
+        if(studenti == null || studenti.length == 0) return false;
+        for(Student s : studenti)
+            if(s.getBrojIndexa().equals(brojIndexa)) return true;
+        return false;
+    }
+
+    private void KreirajPredmet (String nazivPredmeta, Integer sifraPredmeta, int maxBrojStudenata){
+        if(!ProvjeriPodatkePredmeta(nazivPredmeta, sifraPredmeta, maxBrojStudenata)) return;
+        if(predmeti == null || predmeti.length == 0){
+            predmeti = new Predmet[1];
+            predmeti[0] = new Predmet(nazivPredmeta, sifraPredmeta, maxBrojStudenata);
+            return;
+        }
+        Predmet[] novi = new Predmet[predmeti.length + 1];
+        for(int i = 0; i < predmeti.length; i++)
+            novi[i] = predmeti[i];
+        novi[predmeti.length] = new Predmet(nazivPredmeta, sifraPredmeta, maxBrojStudenata);
+        predmeti = novi;
+    }
+    private void DodajStudenta(String prezime, String ime, Integer brojIndexa){
+        if(!ProvjeriPodatkeStudenta(prezime, ime, brojIndexa)) return;
+        if(studenti == null || studenti.length == 0){
+            studenti = new Student[1];
+            studenti[0] = new Student(ime, prezime, brojIndexa);
+            return;
+        }
+        Student[] novi = new Student[studenti.length + 1];
+        for(int i = 0; i < studenti.length; i++)
+            novi[i] = studenti[i];
+        novi[studenti.length] = new Student(ime, prezime, brojIndexa);
+        studenti = novi;
+    }
+    private void UpisiStudentaNaPredmet(Integer brojIndexa, String nazivPredmeta){
+        if(!ImaStudent(brojIndexa)){
+            System.out.println("Student nije nadjen!");
+            return;
+        }
+        for(Student s : studenti)
+            if(s.getBrojIndexa().equals(brojIndexa)) {
+                if(predmeti == null || predmeti.length == 0) {
+                    System.out.println("Nema kreiranih predmeta!");
+                    break;
+                }
+                else
+                    for (Predmet p : predmeti)
+                        if (p.getNazivPredmeta().equalsIgnoreCase(nazivPredmeta)) {
+                            p.Upisi(s);
+                            return;
+                        }
+            }
+    }
+    private void IspisiStudentaSaPredmeta(Integer brojIndexa, String nazivPredmeta){
+        if(!ImaStudent(brojIndexa)){
+            System.out.println("Student nije nadjen!");
+            return;
+        }
+        for(Student s : studenti)
+            if(s.getBrojIndexa().equals(brojIndexa)) {
+                if(predmeti == null || predmeti.length == 0) {
+                    System.out.println("Nema kreiranih predmeta!");
+                    return;
+                }
+                else
+                    for (Predmet p : predmeti)
+                        if (p.getNazivPredmeta().equalsIgnoreCase(nazivPredmeta)) {
+                            p.Ispisi(s);
+                            return;
+                        }
+            }
+    }
+    private void ObrisiStudenta(Integer brojIndexa){
+        if(!ImaStudent(brojIndexa)){
+            System.out.println("Student nije nadjen!");
+            return;
+        }
+        for(Student s : studenti)
+            if(s.getBrojIndexa().equals(brojIndexa))
+                for(Predmet p : predmeti)
+                    if(p.Upisan(s)) p.Ispisi(s);
+        Student[] novi = new Student[studenti.length - 1];
+        int j = 0;
+        for(int i = 0; i < novi.length; i++){
+            if(!studenti[j].getBrojIndexa().equals(brojIndexa)){
+                novi[i] = studenti[j];
+                j++;
+            }
+            else j++;
+        }
+        studenti = novi;
+    }
+    private void ObrisiPredmet(String nazivPredmeta){
+        if(!ImaPredmet(nazivPredmeta)){
+            System.out.println("Predmet nije nadjen!");
+            return;
+        }
+        Predmet[] novi = new Predmet[predmeti.length - 1];
+        int j = 0;
+        for(int i = 0; i < novi.length; i++){
+            if(!predmeti[j].getNazivPredmeta().equalsIgnoreCase(nazivPredmeta)){
+                novi[i] = predmeti[j];
+                j++;
+            }
+            else j++;
+        }
+        predmeti = novi;
+    }
+    private void IspisiSpisakStudenata(String nazivPredmeta){
+        for(Predmet p : predmeti)
+            if(p.getNazivPredmeta().equalsIgnoreCase(nazivPredmeta)){
+                System.out.println(p);
+                return;
+            }
+    }
+
+    public void DodavanjeStudenta(){
+        try{
+            System.out.println("Unesite prezime");
+            Scanner scanner = new Scanner(System.in);
+            String prezime = scanner.nextLine();
+            System.out.println("Unesite ime");
+            scanner = new Scanner(System.in);
+            String ime = scanner.nextLine();
+            System.out.println("Unesite brojIndexa");
+            scanner = new Scanner(System.in);
+            Integer brojIndexa = scanner.nextInt();
+            DodajStudenta(prezime, ime, brojIndexa);
+        } catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    public void KreiranjePredmeta(){
+        try{
+            System.out.println("Unesite naziv predmeta");
+            Scanner scanner = new Scanner(System.in);
+            String nazivPredmeta = scanner.nextLine();
+            System.out.println("Unesite sifru predmeta");
+            scanner = new Scanner(System.in);
+            Integer sifraPredmeta = scanner.nextInt();
+            System.out.println("Unesite maximalan broj studenata na predmetu");
+            scanner = new Scanner(System.in);
+            int maxBrojStudenata = scanner.nextInt();
+            KreirajPredmet(nazivPredmeta, sifraPredmeta, maxBrojStudenata);
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    public void UpisStudentaNaPredmet(){
+        try{
+            System.out.println("Unesite index studenta");
+            Scanner scanner = new Scanner(System.in);
+            Integer brojIndexa = scanner.nextInt();
+            System.out.println("Unesite naziv predmeta");
+            scanner = new Scanner(System.in);
+            String nazivPredmeta = scanner.nextLine();
+            UpisiStudentaNaPredmet(brojIndexa, nazivPredmeta);
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    public void IspisStudentaSaPredmeta(){
+        try{
+            System.out.println("Unesite index studenta");
+            Scanner scanner = new Scanner(System.in);
+            Integer brojIndexa = scanner.nextInt();
+            System.out.println("Unesite naziv predmeta");
+            scanner = new Scanner(System.in);
+            String nazivPredmeta = scanner.nextLine();
+            IspisiStudentaSaPredmeta(brojIndexa, nazivPredmeta);
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    public void BrisanjeStudenta(){
+        try{
+            System.out.println("Unesite index studenta");
+            Scanner scanner = new Scanner(System.in);
+            Integer brojIndexa = scanner.nextInt();
+            ObrisiStudenta(brojIndexa);
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    public void BrisanjePredmeta(){
+        try{
+            System.out.println("Unesite naziv predmeta");
+            Scanner scanner = new Scanner(System.in);
+            String nazivPredmeta = scanner.nextLine();
+            ObrisiPredmet(nazivPredmeta);
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    public void Ispis(){
+        try{
+            System.out.println("Unesite naziv premeta");
+            Scanner scanner = new Scanner(System.in);
+            String nazivPredmeta = scanner.nextLine();
+            IspisiSpisakStudenata(nazivPredmeta);
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        Program program = new Program();
+        program.DodajStudenta("Krdzic", "Nermin", 17825);
+        program.DodajStudenta("Masovic", "Haris", 17696);
+        program.DodajStudenta("xxxxx", "Faris", 17777);
+        program.KreirajPredmet("MLTI", 1, 1);
+        program.KreirajPredmet("RPR", 2, 2);
+        int izbor = 1;
+        while(izbor != 0){
+            System.out.println("Unesite:");
+            System.out.println("1. za, Dodavanje studenta");
+            System.out.println("2. za, Kreiranje predmeta");
+            System.out.println("3. za, Upis studenta na predmet");
+            System.out.println("4. za, Ispis studenta sa predmeta");
+            System.out.println("5. za, Brisanje studenta");
+            System.out.println("6. za, Brisanje predmeta");
+            System.out.println("7. za, Ispis spiska studenata sa navedenog predmeta");
+            System.out.println("0. za, Izlaz iz aplikacije.");
+            Scanner scanner = new Scanner(System.in);
+            izbor = scanner.nextInt();
+            switch (izbor){
+                case 0:
+                    return;
+                case 1:
+                    program.DodavanjeStudenta();
+                    break;
+                case 2:
+                    program.KreiranjePredmeta();
+                    break;
+                case 3:
+                    program.UpisStudentaNaPredmet();
+                    break;
+                case 4:
+                    program.IspisStudentaSaPredmeta();
+                    break;
+                case 5:
+                    program.BrisanjeStudenta();
+                    break;
+                case 6:
+                    program.BrisanjePredmeta();
+                    break;
+                case 7:
+                    program.Ispis();
+                    break;
+            }
+        }
+    }
+}
