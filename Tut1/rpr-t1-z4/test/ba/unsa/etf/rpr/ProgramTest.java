cca126d6433ae0282622c15b8a3d1e7740b1f4f2
Nermin Krdzic <nkrdzic1@etf.unsa.ba>
2018-10-22 16:22:16 +0200

    Dodao neke jUnit testove

---
+++ Tut1/rpr-t1-z4/test/ba/unsa/etf/rpr/ProgramTest.java
@@ -0,0 +1,81 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void provjeriPodatkePredmeta() {
+        
+    }
+
+    @Test
+    void provjeriPodatkeStudenta() {
+    }
+
+    @Test
+    void imaPredmet() {
+    }
+
+    @Test
+    void imaStudent() {
+    }
+
+    @Test
+    void kreirajPredmet() {
+    }
+
+    @Test
+    void dodajStudenta() {
+    }
+
+    @Test
+    void upisiStudentaNaPredmet() {
+    }
+
+    @Test
+    void ispisiStudentaSaPredmeta() {
+    }
+
+    @Test
+    void obrisiStudenta() {
+    }
+
+    @Test
+    void obrisiPredmet() {
+    }
+
+    @Test
+    void ispisiSpisakStudenata() {
+    }
+
+    @Test
+    void dodavanjeStudenta() {
+    }
+
+    @Test
+    void kreiranjePredmeta() {
+    }
+
+    @Test
+    void upisStudentaNaPredmet() {
+    }
+
+    @Test
+    void ispisStudentaSaPredmeta() {
+    }
+
+    @Test
+    void brisanjeStudenta() {
+    }
+
+    @Test
+    void brisanjePredmeta() {
+    }
+
+    @Test
+    void ispis() {
+    }
+}
